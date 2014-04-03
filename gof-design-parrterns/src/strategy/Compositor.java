82f0f5838942933d69d82c8f33d6bb382ce14ba3
Kien <kientrantrung.code@gmail.com>
2014-04-03 13:49:40 +0200

    changes in Compositor

--- gof-design-parrterns/src/strategy/Compositor.java
+++ gof-design-parrterns/src/strategy/Compositor.java
@@ -1,6 +1,17 @@
 package strategy;
 
 public interface Compositor {
-	public int Compose(Coord[] natural, Coord[] stretch, Coord[] shrink,
+	/**
+	 * 
+	 * @param natural
+	 * @param stretch
+	 * @param shrink
+	 * @param componentCount
+	 * @param lineWidth
+	 * @param breaks
+	 * @return
+	 */
+	public int compose(Coord[] natural, Coord[] stretch, Coord[] shrink,
 			int componentCount, int lineWidth, int[] breaks);
+	
 }
